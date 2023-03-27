be630a52abf8dd2c4972e6aade85eeb8c069040e
Ingrid <ingrid.schwarz.wcs@gmail.com>
2023-03-27 15:38:37 +0200

    correction router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React, { Component } from 'react';
 import TitlePart from './components/TitlePart';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Routes, NavLink} from 'react-router-dom';
 import './App.css';
 import Speedrun from './components/Speedrun';
 
 class App extends Component {
+  
   render() {   
-    return (
-      <div className="App">
-        <header>
-          <nav>
-              <h2>Test barre de navigation</h2>  
-              <ul>
-                  <li><NavLink activeClassName="navlink" to="/">Accueil</NavLink></li>
-                  <li><NavLink activeClassName="navlink" to="/jeux">Les jeux</NavLink></li>
-                  <li><NavLink activeClassName="navlink" to="/mes-jeux">Mes jeux</NavLink></li>
-                  <li><NavLink activeClassName="navlink" to="/speedrun">Speedruns</NavLink></li>
-              </ul>
-          </nav>
-          <BrowserRouter>
+    return (     
+      <BrowserRouter> 
+        <div className="App">
+          <Routes>
             <Route exact path="/" element={TitlePart}></Route>
             <Route exact path="/speedruns" element={Speedrun}></Route>
-          </BrowserRouter>
-        </header>
-      </div>
+          </Routes>
+          <header>
+            <nav>
+                <h2>Test barre de navigation</h2>  
+                <ul>
+                    <li><NavLink activeClassName="navlink" to="/">Accueil</NavLink></li>
+                    <li><NavLink activeClassName="navlink" to="/jeux">Les jeux</NavLink></li>
+                    <li><NavLink activeClassName="navlink" to="/mes-jeux">Mes jeux</NavLink></li>
+                    <li><NavLink activeClassName="navlink" to="/speedrun">Speedruns</NavLink></li>
+                </ul>
+            </nav>
+            
+          </header>
+        </div>
+      </BrowserRouter>
     );
   }
 }
