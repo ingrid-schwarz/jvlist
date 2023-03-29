70ba739754c7328a6553e206b69c02320b8e03ae
Ingrid <ingrid.schwarz.wcs@gmail.com>
2023-03-29 16:15:22 +0200

    css et pages jeux et mes jeux fonctionnelle

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import './css/TitlePart.css';
+import './css/Game.css';
 import Nes from '../images/nes-controller.png';
 import Panda from '../images/panda.png';
 import Rocket from '../images/rocket.png';
 
 function Games (){
-
-
+    return(
+        <div className='game'>
+            <h1>Jeux Vidéo</h1>
+            <img src={Nes}/>
+        </div>
+    );
 }
 
 export default Games;
